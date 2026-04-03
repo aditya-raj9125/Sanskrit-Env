61bd0ed134ab4cbb661dc95372980a1db3b1103c
Mitansh-09 <mitanshdev09@gmail.com>
2026-04-03 10:16:28 +0530

    Add SamasaGrader to module exports

--- graders/__init__.py
+++ graders/__init__.py
@@ -1,5 +1,6 @@
 from .glossary_grader import GlossaryGrader
 from .sandhi_grader import SandhiGrader
 from .coherence_grader import CoherenceGrader
+from .samasa_grader import SamasaGrader
 
-__all__ = ["GlossaryGrader", "SandhiGrader", "CoherenceGrader"]
+__all__ = ["GlossaryGrader", "SandhiGrader", "CoherenceGrader", "SamasaGrader"]
